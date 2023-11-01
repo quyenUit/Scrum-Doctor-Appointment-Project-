776ec2804e37e41b5420535aea320d52e9313b8a
Nguyễn Hữu Quyến <quyen-nh-intern@aipower.vn>
2023-11-02 00:40:11 +0700

    code css

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
 import SymptomsInput from '../components/SymptomsInput'
+import '../assets/css/HomePage.css'
 
 const HomePage = () => {
   return (
     <div>
+       <h1 className='title_appointment'>Doctor Appointment System</h1>
+       
         <SymptomsInput currentPage="SymptomsInput" />
     </div>
   )
