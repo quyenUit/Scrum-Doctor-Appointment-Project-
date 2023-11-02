507b8bea9212288bb303a753fc2211e96b7db622
ThienNguyen <thienenpi280220@gmail.com>
2023-11-02 09:15:54 +0700

    update date format

--- src/components/SetAppointment.jsx
+++ src/components/SetAppointment.jsx
@@ -16,11 +16,12 @@ const SetAppointment = () => {
 
   const handleDateChange = (date) => {
     setStartDate(date);
-    const day = String(date.getDate()).padStart(2, '0');
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const year = date.getFullYear();
-    const dateString = `${day}/${month}/${year}`;
-    console.log(dateString);
+    // const day = String(date.getDate()).padStart(2, '0');
+    // const month = String(date.getMonth() + 1).padStart(2, '0');
+    // const year = date.getFullYear();
+    // const dateString = `${day}/${month}/${year}`;
+    // console.log(dateString);
+    console.log(date)
   };
   const handleTimeChange = (time) => {
     setSelectedTime(time.target.value);
